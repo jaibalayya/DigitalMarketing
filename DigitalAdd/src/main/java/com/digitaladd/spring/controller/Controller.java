c38e64bd9a8e6c1eba229453d84259e3c1de27bf
tejav <tejav@DESKTOP-MUNHRGU>
2017-10-11 01:06:38 +0530

    by amiya

---
+++ DigitalAdd/src/main/java/com/digitaladd/spring/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.digitaladd.spring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	@GetMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("name", "John Doe");
+
+		return "welcome";
+	}
+}
